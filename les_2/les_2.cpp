a7249f03e70d4cb5ba506bfd7cfb75ea910c5a87
ViktoriaSavosta <60658327+ViktoriaSavosta@users.noreply.github.com>
2020-03-26 22:33:54 +0200

    First commit

---
+++ les_2/les_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    float i = 0;
+
+    std::cout << " Enter value : ";
+    std::cin >> i;
+
+    float result = i / 1000;
+    std::cout << " Result : " << result << std::endl;
+
+    return 0;
+}
+
